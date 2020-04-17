ad0499b61e6a834db46d819c06faba10a380972d
Jose Cabrera <jcabrera@habitissimo.com>
2020-04-17 09:55:19 +0200

    master: format dates by our own fn

--- react-corporate/src/components/Booking/BookingDates.js
+++ react-corporate/src/components/Booking/BookingDates.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 class BookingDates extends Component {
   formatDate(date) {
-    const month = '' + (date.getMonth() + 1);
-    const day = '' + date.getDate();
+    let month = '' + (date.getMonth() + 1);
+    let day = '' + date.getDate();
     const year = date.getFullYear();
 
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
-    return [month, day, year].join('/');
+    return [year, month, day].join('-');
   }
 
   render() {
@@ -18,7 +18,10 @@ class BookingDates extends Component {
         <label>Check-in</label>
         <input type="date" defaultValue={this.formatDate(this.props.checkIn)} />
         <label>Check-out</label>
-        <input type="date" defaultValue={this.formatDate(this.props.checkOut)} />
+        <input
+          type="date"
+          defaultValue={this.formatDate(this.props.checkOut)}
+        />
       </section>
     );
   }
